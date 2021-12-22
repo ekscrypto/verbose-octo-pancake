239cb972cb172bd45295d2310e4b1b2bcb179f56
Dave Poirier <dave@encoded.life>
2021-12-21 20:04:59 -0500

    Created API Secrets file

---
+++ Yelp Search/Yelp Search/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  Yelp Search
+//
+//  Created by Dave Poirier on 2021-12-21.
+//
+
+import Foundation
+
+enum Secrets {
+    static let yelpApiKey = "{{PIPELINE_INJECTED_YELP_API_KEY}}"
+}
